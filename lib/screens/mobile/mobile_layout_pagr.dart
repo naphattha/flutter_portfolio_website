51f3fb0bc4eeefe4d449265c7dc724b43598faaa
Naphattha <51052596+naphattha@users.noreply.github.com>
2025-12-21 22:04:11 +0700

    count_container_widget
    build count_container_widget for mobile and tablet layout

--- lib/screens/mobile/mobile_layout_pagr.dart
+++ lib/screens/mobile/mobile_layout_pagr.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio_website/constants/colors.dart';
 import 'package:flutter_portfolio_website/constants/styles.dart';
 import 'package:flutter_portfolio_website/screens/tablet/tablet_layout_pagr.dart';
+import 'package:flutter_portfolio_website/screens/widgets/count_container_widget.dart';
 import 'package:flutter_portfolio_website/screens/widgets/header_text_widget.dart';
 import 'package:flutter_portfolio_website/screens/widgets/rotating_image_widget.dart';
 
@@ -50,6 +52,40 @@ class _MobileLayoutState extends State<MobileLayout> {
                     ),
                   ],
                 ),
+                SizedBox(height: 20,),
+                Container(
+                  width: size.width,
+                  margin: EdgeInsets.symmetric(horizontal: size.width*0.05),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CountWidget(size: size, text1: "0→1", text2: "From", text3: "Learning to Building"),
+                      const SizedBox(height: 10,),
+                      Divider(
+                        color: AppColors.gray600,
+                        indent: size.width*0.05,
+                        endIndent: size.width*0.05,
+                      ),
+                      CountWidget(size: size, text1: "Clean", text2: "Code &", text3: "Architecture"),
+                      const SizedBox(height: 10,),
+                      Divider(
+                        color: AppColors.gray600,
+                        indent: size.width*0.05,
+                        endIndent: size.width*0.05,
+                      ),
+                      CountWidget(size: size, text1: "Flutter", text2: "State", text3: "Management"),
+                      const SizedBox(height: 10,),
+                      Divider(
+                        color: AppColors.gray600,
+                        indent: size.width*0.05,
+                        endIndent: size.width*0.05,
+                      ),
+                      CountWidget(size: size, text1: "Real", text2: "World", text3: "Projects"),
+                    ],
+                  ),
+                ),
+
               ],
             ),
           ),
